7fb428e39d42a6d9d43052e570bb343460dbbd21
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-31 19:38:37 +1100

    styled active NavLinks

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,24 @@
+import styles from "./NavBar.module.scss";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const linkStyles = ({ isActive }) => {
+    return isActive
+      ? `${styles.nav__link} ${styles.nav__link__active}`
+      : `${styles.nav__link}`;
+  };
+
   return (
-    <nav>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/skills">Skills</NavLink>
-      <NavLink to="/projects">Projects</NavLink>
+    <nav className={styles.nav}>
+      <NavLink className={linkStyles} to="/about">
+        About
+      </NavLink>
+      <NavLink className={linkStyles} to="/skills">
+        Skills
+      </NavLink>
+      <NavLink className={linkStyles} to="/projects">
+        Projects
+      </NavLink>
     </nav>
   );
 };
