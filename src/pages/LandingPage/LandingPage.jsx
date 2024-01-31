f441a015bab2f0192b87df564a7c1fc23bcb85fc
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-01-31 22:31:55 +1100

    added content to landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,28 @@
+import styles from "./LandingPage.module.scss";
+import MainWrapper from "../../containers/MainWrapper/MainWrapper";
+
 const LandingPage = () => {
-  return <main>LandingPage</main>;
+  return (
+    <MainWrapper>
+      <div className={styles.wrapper}>
+        <img
+          className={styles.img}
+          src="../src/assets/self-portrait.png"
+          alt="Marielle Lopez"
+        />
+        <div className={styles.intro}>
+          <p className={styles.intro__greeting}>Hi, I'm</p>
+          <h1 className={styles.intro__heading}>
+            <span className={styles.intro__heading__opening_tag}>&lt;</span>
+            Marielle Lopez
+            <span className={styles.intro__heading__closing_tag}>/&gt;</span>
+            <span className={styles.intro__heading__cursor}></span>
+          </h1>
+        </div>
+      </div>
+      <p className={styles.role}>Junior Full-stack Developer // Dance Coach</p>
+    </MainWrapper>
+  );
 };
 
 export default LandingPage;
