ac9bd653beefa03cd326050f4de608bbea0e9e04
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-01 17:20:17 +1100

    created PageLine component

--- src/components/PageLine/PageLine.jsx
+++ src/components/PageLine/PageLine.jsx
@@ -1,6 +1,6 @@
 import styles from "./PageLine.module.scss";
 
-const PageLine = ({ title }) => {
+const PageLine = ({ title = "Section heading" }) => {
   return (
     <div className={styles.page_line}>
       <p className={styles.page_line__heading}>{title}</p>
