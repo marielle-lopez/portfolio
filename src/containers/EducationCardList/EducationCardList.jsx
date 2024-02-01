1acf88c658b9eb24c20c3af4426fe6d56161192a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-01 17:20:44 +1100

    created EducationCard container

---
+++ src/containers/EducationCardList/EducationCardList.jsx
@@ -0,0 +1,32 @@
+import styles from "./EducationCardList.module.scss";
+import data from "../../data/education.json";
+import EducationCard from "../../components/EducationCard/EducationCard";
+
+const EducationCardList = () => {
+  return (
+    <div className={styles.education_card_container}>
+      {data.map((item, i) => {
+        return (
+          <>
+            <EducationCard
+              key={item.id}
+              startYear={item.startYear}
+              endYear={item.endYear}
+              img={item.img}
+              desc={item.description}
+            />
+            {i !== data.length - 1 && (
+              <img
+                className={styles.education_card_container__star_separator}
+                src="../src/assets/star.png"
+                alt="Star"
+              />
+            )}
+          </>
+        );
+      })}
+    </div>
+  );
+};
+
+export default EducationCardList;
