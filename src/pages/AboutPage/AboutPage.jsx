d53bc77b830e85d9de6443fe05ec8e1ce7e34f1a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-01 17:21:19 +1100

    added education info to About page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,12 +1,14 @@
 import MainWrapper from "../../containers/MainWrapper/MainWrapper";
 import PageHeading from "../../components/PageHeading/PageHeading";
 import PageLine from "../../components/PageLine/PageLine";
+import EducationCardList from "../../containers/EducationCardList/EducationCardList";
 
 const AboutPage = () => {
   return (
     <MainWrapper>
       <PageHeading title="I'm Marielle." />
       <PageLine title="Education" />
+      <EducationCardList />
       <PageLine title="A little bit more" />
     </MainWrapper>
   );
