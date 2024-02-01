35f10a6ce750c619fa53aa528308ed55662163bd
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-01 16:00:10 +1100

    implemented PageHeading component

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,7 +1,12 @@
 import MainWrapper from "../../containers/MainWrapper/MainWrapper";
+import PageHeading from "../../components/PageHeading/PageHeading";
 
 const AboutPage = () => {
-  return <MainWrapper>AboutPage</MainWrapper>;
+  return (
+    <MainWrapper>
+      <PageHeading title="I'm Marielle." />
+    </MainWrapper>
+  );
 };
 
 export default AboutPage;
