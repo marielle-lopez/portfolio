43f545abdc5e8862d0ab19b49efe05cd18718244
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-01 18:14:03 +1100

    added extended about me section on About page

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,14 +2,18 @@ import MainWrapper from "../../containers/MainWrapper/MainWrapper";
 import PageHeading from "../../components/PageHeading/PageHeading";
 import PageLine from "../../components/PageLine/PageLine";
 import EducationCardList from "../../containers/EducationCardList/EducationCardList";
+import ExtendedAboutContainer from "../../containers/ExtendedAboutContainer/ExtendedAboutContainer";
 
 const AboutPage = () => {
   return (
     <MainWrapper>
       <PageHeading title="I'm Marielle." />
+
       <PageLine title="Education" />
       <EducationCardList />
+
       <PageLine title="A little bit more" />
+      <ExtendedAboutContainer />
     </MainWrapper>
   );
 };
