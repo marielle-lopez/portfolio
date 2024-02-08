58a867af7ebc7b82f428814e0ac4d6761a2c8860
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-08 22:34:58 +1100

    implemented SkillCard component and container on Skills page with data

--- src/pages/SkillsPage/SkillsPage.jsx
+++ src/pages/SkillsPage/SkillsPage.jsx
@@ -1,7 +1,15 @@
 import MainWrapper from "../../containers/MainWrapper/MainWrapper";
+import PageHeading from "../../components/PageHeading/PageHeading";
+import SkillCardList from "../../containers/SkillCardList/SkillCardList";
+import data from "../../data/skills.json";
 
 const SkillsPage = () => {
-  return <MainWrapper>SkillsPage</MainWrapper>;
+  return (
+    <MainWrapper>
+      <PageHeading title="Skills" />
+      {data && <SkillCardList data={data} />}
+    </MainWrapper>
+  );
 };
 
 export default SkillsPage;
