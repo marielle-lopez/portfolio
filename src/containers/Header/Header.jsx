4f18bd511c771580682e7349810bf1df9c7fc095
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-11 16:59:38 +1100

    added media queries for Header component for small phone

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -16,60 +16,85 @@ const Header = () => {
 
   return (
     <animated.header className={styles.header} style={{ ...springs }}>
-      <div className={[styles.header__section, styles.header__left].join(" ")}>
-        <NavBar />
-      </div>
-
-      <div
-        className={[styles.header__section, styles.header__centre].join(" ")}
-      >
+      <div className={styles.header_small}>
         <NavLink to="/">
           <img
-            className={styles.header__logo}
+            className={styles.header_small__logo}
             src="../src/assets/logo.png"
             alt="Marielle Lopez logo"
           />
         </NavLink>
       </div>
 
-      <div className={[styles.header__right, styles.header__section].join(" ")}>
-        <div className={styles.header__right__external_links}>
-          <Link
-            className={styles.header__right__link}
-            to="https://github.com/marielle-lopez"
-            target="_blank"
-          >
-            <img
-              className={styles.header__right__icon}
-              src="../src/assets/github.png"
-              alt="GitHub icon"
-            />
-          </Link>
-          <Link
-            className={styles.header__right__link}
-            to="https://www.linkedin.com/in/marielle-lopez/"
-            target="_blank"
-          >
-            <img
-              className={styles.header__right__icon}
-              src="../src/assets/linkedin.png"
-              alt="LinkedIn icon"
-            />
-          </Link>
-          <img
-            className={styles.header__right__icon}
-            src="../src/assets/email.png"
-            alt="Email icon"
-          />
+      <div className={styles.header_large}>
+        <div
+          className={[
+            styles.header_large__section,
+            styles.header_large__left,
+          ].join(" ")}
+        >
+          <NavBar />
         </div>
-        <button className={styles.header__btn}>
-          Download CV
-          <img
-            className={styles.header__btn__icon}
-            src="../src/assets/download.png"
-            alt="Download icon"
-          />
-        </button>
+
+        <div
+          className={[
+            styles.header_large__section,
+            styles.header_large__centre,
+          ].join(" ")}
+        >
+          <NavLink to="/">
+            <img
+              className={styles.header_large__logo}
+              src="../src/assets/logo.png"
+              alt="Marielle Lopez logo"
+            />
+          </NavLink>
+        </div>
+
+        <div
+          className={[
+            styles.header_large__right,
+            styles.header_large__section,
+          ].join(" ")}
+        >
+          <div className={styles.header_large__right__external_links}>
+            <Link
+              className={styles.header_large__right__link}
+              to="https://github.com/marielle-lopez"
+              target="_blank"
+            >
+              <img
+                className={styles.header_large__right__icon}
+                src="../src/assets/github.png"
+                alt="GitHub icon"
+              />
+            </Link>
+            <Link
+              className={styles.header_large__right__link}
+              to="https://www.linkedin.com/in/marielle-lopez/"
+              target="_blank"
+            >
+              <img
+                className={styles.header_large__right__icon}
+                src="../src/assets/linkedin.png"
+                alt="LinkedIn icon"
+              />
+            </Link>
+            <img
+              className={styles.header_large__right__icon}
+              src="../src/assets/email.png"
+              alt="Email icon"
+            />
+          </div>
+          <button className={styles.header_large__btn}>
+            Download CV
+            <img
+              className={styles.header_large__btn__icon}
+              src="../src/assets/download.png"
+              alt="Download icon"
+            />
+          </button>
+        </div>
       </div>
     </animated.header>
   );
