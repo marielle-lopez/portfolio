affea0f933d845b90d2cbf30aab0699ccd5f557a
marielle-lopez <92203731+marielle-lopez@users.noreply.github.com>
2024-02-11 12:49:13 +1100

    implemented ProjectCard component and related container

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,7 +1,15 @@
 import MainWrapper from "../../containers/MainWrapper/MainWrapper";
+import PageHeading from "../../components/PageHeading/PageHeading";
+import ProjectCardList from "../../containers/ProjectCardList/ProjectCardList";
+import data from "../../data/projects.json";
 
 const ProjectsPage = () => {
-  return <MainWrapper>ProjectsPage</MainWrapper>;
+  return (
+    <MainWrapper>
+      <PageHeading title="Projects" />
+      <ProjectCardList data={data} />
+    </MainWrapper>
+  );
 };
 
 export default ProjectsPage;
